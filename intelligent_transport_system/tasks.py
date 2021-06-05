ddbe6e39d5662a8869a89966dbe0d0e9890836c8
tuanvuPresent <tuanvubk96@gmail.com>
2021-06-05 14:32:04 +0000

    fix speed vehice

--- intelligent_transport_system/tasks.py
+++ intelligent_transport_system/tasks.py
@@ -2,7 +2,9 @@ import logging
 from pyicloud import PyiCloudService
 from django.conf import settings
 from apps.vehicle.models import TrackVehicle, Vehicle
-from datetime import datetime
+from datetime import datetime, timedelta
+from geopy.distance import geodesic
+from django.db.models import Prefetch
 
 def tracking_vehicle():
     logger = logging.getLogger()
@@ -14,14 +16,27 @@ def tracking_vehicle():
         location = api.iphone.location()
         lat, lng = location.get('latitude'), location.get('longitude')
         now = datetime.now()
-        vehicle_id = Vehicle.objects.all().first()
-        if vehicle_id:
+        vehicle = Vehicle.objects.all().prefetch_related(
+            Prefetch(
+                'trackvehicle_set',
+                queryset=TrackVehicle.objects.all().order_by('-date'),
+            )
+        ).first()
+        if vehicle:
+            trackvehicle = vehicle.trackvehicle_set.all()
+            speed = 0
+            if len(trackvehicle) > 0:
+                localtion_last = trackvehicle[0].latitude, trackvehicle[0].longitude
+                t1 = trackvehicle[0].date.timestamp()
+                t2 = datetime.now().astimezone().timestamp()
+                if t2 - t1 < 20:
+                    speed = 3.6 * geodesic(localtion_last, (lat, lng)).meters / (t2 - t1)
             TrackVehicle.objects.create(
                 date=now,
                 longitude=lng,
                 latitude=lat,
-                vehicle_id=vehicle_id,
-                speed=0
+                vehicle_id=vehicle,
+                speed=speed
             )
     except:
         pass
