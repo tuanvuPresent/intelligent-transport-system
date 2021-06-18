d570d4825848eeeb262152813db42e9d21f3b2f4
tuanvuPresent <tuanvubk96@gmail.com>
2021-06-18 12:59:42 +0000

    update

--- apps/vehicle/views.py
+++ apps/vehicle/views.py
@@ -106,7 +106,7 @@ class VehicleLocaltionAPIView(BaseModelViewSet):
         queryset = Vehicle.objects.all().select_related('owner_id').prefetch_related(
             Prefetch(
                 'trackvehicle_set',
-                queryset=TrackVehicle.objects.filter(date__date=date).order_by('-date'),
+                queryset=TrackVehicle.objects.filter(date__date=date, date__lte=datetime.now()).order_by('-date'),
             )
         )
         queryset = get_object_or_404(queryset, pk=id)
